e08632dacca781975a0f87d2903e2aa19a181663
gwaihir49 <131680098+gwaihir49@users.noreply.github.com>
2026-07-25 11:56:35 +0200

    Reapply "level 3 fix"
    This reverts commit 832927f1cc05a1561f306ccfbc8dfbb631a92390.

--- GMTK_26/Assets/Scripts/UI_scripts/CountDown.cs
+++ GMTK_26/Assets/Scripts/UI_scripts/CountDown.cs
@@ -26,7 +26,7 @@ public class CountDown : MonoBehaviour
         }
         timeFlowMultiplier = 1;
 
-        animator = GameObject.Find("Player").GetComponentInChildren<Animator>();
+        animator = FindFirstObjectByType<PlayerMovement>(FindObjectsInactive.Include).transform.GetComponentInChildren<Animator>(true);
     }
 
     void FixedUpdate()
